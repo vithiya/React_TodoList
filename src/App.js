06b641c6ced57424246ef03d6ec11be4adfd7dc0
vithiya <86958980+vithiyanwsdb@users.noreply.github.com>
2023-08-03 13:34:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
+import './index.css';
+import { useEffect } from 'react';
+import Form from './Form';
+import { List } from './List';
 
 function App() {
+const API_URL='https://jsonplaceholder.typicode.com/';
+
+const [reqType,setReqType]=useState('users');
+const [items,setItems]=useState([]);
+
+useEffect(()=>{
+    const fetchItems = async () =>{
+      try{
+        const response = await fetch(`${API_URL}${reqType}`);
+        console.log(`${API_URL}${reqType}`)
+        console.log(response);
+        const data = await response.json();
+        console.log(data);
+        setItems(data);
+      }catch(err){
+         console.log(err)
+      }
+      
+    }
+    fetchItems();
+},[reqType])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form 
+        reqType={reqType}
+        setReqType={setReqType}/>
+      <List items={items}/>
     </div>
   );
 }
---
+++ src/List.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ListItem from './ListItem'
+
+export const List = ({items}) => {
+  return (
+    (<ul>
+        {
+            items.map((item) =><ListItem key={item.id} item={item}/> )
+        }
+    </ul>)
+  )
+}
